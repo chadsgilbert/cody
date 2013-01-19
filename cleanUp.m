715b576bd4f5827e0a9efbf92e77fe35ed5ecd2e
chad <chad.gilbert@forces.gc.ca>
2013-01-19 10:00:58 -0400

    Small improvement to cleanUp (p17)

--- cleanUp.m
+++ cleanUp.m
@@ -1,6 +1,12 @@
-function y = cleanUp(x)
+function x = cleanUp(x)
+%  Given an input vector x, find all elements of x less than 0 and greater than
+%  10 and replace them with NaN.
+%
+%  Example:
+%
+%    Input  x =  [  5  17 -20  99  3.4  2  8  -6 ]
+%    Output y is [  5 NaN NaN NaN  3.4  2  8 NaN ]
 
-y = x;
-y(y<0 | y>10) = NaN;
+x(x<0 | x>10) = NaN;
 
 end
